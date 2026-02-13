9a7a06139bc21eb10226ac2ada3c8aa819430bf0
Tahir <tahir@example.com>
2026-02-13 15:18:46 +0300

    build: Split node_modules into separate vendor chunk for better performance

--- vite.config.js
+++ vite.config.js
@@ -70,7 +70,16 @@ export default defineConfig({
     })
   ],
   build: {
-    chunkSizeWarningLimit: 1500,
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
 
